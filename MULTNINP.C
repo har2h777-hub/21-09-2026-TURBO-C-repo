91a2889457c7d5cca8cca0a95e5a579762dc8b8d
har2h777-hub <har2h777@gmail.com>
2026-09-22 00:02:27 +0530

    Add files via upload

---
+++ MULTNINP.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    clrscr();
+    printf("enter number 1:");
+    scanf("%d",&a);
+    printf("enter number 2:");
+    scanf("%d",&b);
+    c=a*b;
+    printf("product is:=%d",c);
+    getch();
+    return 0;
+}
